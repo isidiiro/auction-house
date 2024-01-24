7e86830d162d24bebe57ad73341b71d000fc5948
isidiiro <sr9958043126@gmail.com>
2024-01-24 23:24:04 +0530

    added the send otp for email verification and satrted working on login part

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { userSignup, userLogin, verifyOtp } = require('../controllers/userContollers');
+const { userSignup, userLogin, verifyOtp, sendEmailOtp } = require('../controllers/userContollers');
 const router = express.Router();
 
 router.post('/signup', userSignup);
-router.post("/verify-email", verifyOtp);
+router.post("/verify-otp", verifyOtp);
 router.post('/login', userLogin);
+router.post('/send-otp', sendEmailOtp)
 
 module.exports = router;
